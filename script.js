b3861d49017cc3a775fa9126509b75d6c534b655
Prateek Parmar <prateekparmar0@gmail.com>
2021-03-28 14:31:17 +0530

    Random Quote project added

---
+++ script.js
@@ -0,0 +1,62 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const facebookBtn = document.getElementById("facebook");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+let apiQuotes = [];
+
+//show new quote
+function newQuote() {
+  loading();
+  let quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.text;
+  complete();
+}
+// Get quotes from API
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://type.fit/api/quotes"; //constant api url
+  try {
+    //Try block to test a block of code for errors
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    //await  makes the code look synchronous by blocking execution of code that follows it until the promise is fulfilled//
+    newQuote();
+  } catch (error) {}
+}
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+function sharePost() {
+  let url = "https://www.brainyquote.com/topics/url-quotes";
+  const facebookUrl = `https://www.facebook.com/sharer.php?u=${url}`;
+  window.open(facebookUrl, "_blank");
+}
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}-${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+facebookBtn.addEventListener("click", sharePost);
+//onLoad
+getQuotes();
